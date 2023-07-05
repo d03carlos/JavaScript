09ae6ff9fc84705e1ebbf97599d21d24773972f9
fidel Laura <lsfidel2@gmail.com>
2023-07-05 00:37:30 -0500

    primer comiit

---
+++ app.js
@@ -0,0 +1,11 @@
+let nombre = "Juan";
+
+console.log(nombre);
+let numero1 = 12;
+let numero2 = 12.5;
+let suma = numero1 + numero2;
+
+console.log(suma);
+for(i=1;i<=10;i++){
+    console.log(i);
+}
