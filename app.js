a6f5f8287af1dbfe0927f6a828a0b617b87086c9
fidel laura <lsfidel@gmail.com>
2023-08-11 20:48:51 -0500

    resolv conflic

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
-let nombre = "Juan";
+let nombre = "Hola Mundo JS";
 
 console.log(nombre);
-let numero1 = 12;
+let numero1 = 12.3;
 let numero2 = 12.5;
 let suma = numero1 + numero2;
 
+let nom1 = "jana Cova";
 console.log(suma);
-for(i=1;i<=10;i++){
+
+console.log(nom1);
+for (i = 1; i <= 10; i++) {
     console.log(i);
+}
+if (suma > 30) {
+    console.log('el numero es mayor a: ' + suma)
+}
+else {
+    console.log('el numero es: ' + suma)
+}
+
+let diaSemana = 'martes';
+switch (diaSemana) {
+    case 'lunes':
+        console.log("es el primer dia de la semana");
+        break;
+    case 'martes':
+        console.log('El dia es martes');
+        break;
+    case "miercoles":
+        console.log("el dia es miercoles")
+        break;
+    default:
+        console.log('Dia no valido');
+        break;
 }
